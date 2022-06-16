c166b94662659475282755a87a129778b19e2ac3
Omarali1020 <93990428+Omarali1020@users.noreply.github.com>
2022-06-16 21:43:32 +0500

    java set up

--- Java/data.java
+++
@@ -1,4 +0,0 @@
-    var number1 =20
-    var number2 =30
-    var result =number1+number2
-    console.log(result)
---
+++ Java/data.js
@@ -0,0 +1,4 @@
+var number1 =20
+var number2 =30
+var result =number1+number2
+console.log(result)
